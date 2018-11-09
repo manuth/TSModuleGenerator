373ca6602f921ebfb42b501721d6fe2db5fd69fb
Manuel Thalmann <m@nuth.ch>
2018-11-09 10:59:59 +0100

    Add a generator for node-modules

---
+++ src/generators/app/ModuleComponent.ts
@@ -0,0 +1,15 @@
+/**
+ * Represents a component of a module.
+ */
+export enum ModuleComponent
+{
+    /**
+     * Indicates the `TSLint`-component.
+     */
+    TSLint = "tslint",
+
+    /**
+     * Indicates the `VSCode`-component.
+     */
+    VSCode = "vscode"
+}
---
+++ src/generators/app/ModuleSetting.ts
@@ -0,0 +1,30 @@
+/**
+ * Specifies a setting.
+ */
+export enum ModuleSetting
+{
+    /**
+     * Indicates the `Destination`-setting.
+     */
+    Destination = "destination",
+
+    /**
+     * Indicates the `Name` setting.
+     */
+    Name = "moduleName",
+
+    /**
+     * Indicates the `DisplayName`-setting.
+     */
+    DisplayName = "name",
+
+    /**
+     * Indicates the `Description`-setting.
+     */
+    Description = "description",
+
+    /**
+     * Indicates the `LintMode`-setting.
+     */
+    LintMode = "lintMode"
+}
